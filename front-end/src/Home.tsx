e4091bc03d3d85afac72238a8f1e1813ce2d490e
kliu3115 <kliu3115@gmail.com>
2024-07-31 13:34:05 -0400

    changed framework from other to next.js

--- front-end/src/Home.tsx
+++ front-end/src/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <div className = "home">
             <h1> Quiz Corrections </h1>
-            <h2> Website that allows you to create sets and study your past quiz corrections. </h2>
+            <h2> Website that allows you to create sets and study your quiz corrections. </h2>
         </div>
     )
 }
